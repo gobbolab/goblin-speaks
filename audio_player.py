59b29a9f2ec7c43b1a396e7f6d237b2fa9a2155b
James Hogle <jhogle117@gmail.com>
2026-06-10 22:49:30 -0500

    up pygame mixer audio buffer

--- audio_player.py
+++ audio_player.py
@@ -4,6 +4,7 @@ import pygame
 
 class AudioPlayer:
     def __init__(self):
+        pygame.mixer.pre_init(44100, -16, 1, 2048)
         if not pygame.mixer.get_init():
             pygame.mixer.init()
         
