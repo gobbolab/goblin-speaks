a2a284a670e49003bd444623abd7fa489d301eb5
James Hogle <jhogle117@gmail.com>
2026-06-13 18:54:07 -0500

    move animatronic logic to package

--- src/animatronic/gs_body.py
+++ src/animatronic/gs_body.py
@@ -1,16 +1,53 @@
 from .base import Animatronic
+from adafruit_servokit import ServoKit
+import threading
+
+mouth_movement_delay = 0.2
+mouth_closed_angle = 70
+mouth_open_angle = 180
+
+arm_start = 0
+arm_end = 10
+arm_duration = 0.5
+arm_steps = 200
+arm_delay = 1
 
 class GSBody(Animatronic):
     def __init__(self, arm_pin, mouth_pin, *args, **kwargs):
         """
         Initializes the GSBody with arm and mouth servo pins.
         """
+        kit = ServoKit(channels=16)
+
+        self.arm = kit.servo[arm_pin]
+        self.mouth = kit.servo[mouth_pin]
+        
 
     def animate(self, duration: float):
         """
         Performs animation logic over the specified duration.
         """
-        print(f"Animating body for {duration} seconds...")
+        print(f"Animating GS body for {duration} seconds...")
+
+        talk_thread = threading.Thread(
+            target=self.__animate_mouth, 
+            args=(duration,), 
+            daemon=True
+        )
+
+        arm_thread = threading.Thread(
+            target=self.__animate_arm,
+            args=(duration,),
+            daemon=True
+        )
+        
+        talk_thread.start()
+        arm_thread.start()
+            
+        talk_thread.join()
+        arm_thread.join()
+        
+        print("Animation complete!")
 
     def test(self):
         """
@@ -18,3 +55,31 @@ class GSBody(Animatronic):
         """
         print("Testing arm and mouth servos...")
         return True
+
+    def __animate_mouth(self, duration):
+        print("Starting mouth animation...")
+        start_time = time.time()
+        
+        while (time.time() - start_time) < duration:
+            self.mouth.angle = mouth_open_angle
+            time.sleep(mouth_movement_delay)
+            self.mouth.angle = mouth_closed_angle
+            time.sleep(mouth_movement_delay)
+
+    def __animate_arm(self, duration):
+        print("Starting arm animation...")
+        start_time = time.time()
+        
+        while (time.time() - start_time) < duration:
+            self.__smooth_servo_movement(self.arm, arm_start, arm_end, arm_duration, arm_steps)
+            time.sleep(1)
+            self.__smooth_servo_movement(self.arm, arm_end, arm_start, arm_duration, arm_steps)
+            time.sleep(1)
+
+    def __smooth_servo_movement(self, servo, start_angle, end_angle, duration, steps=50):
+        angle_increment = (end_angle - start_angle) / steps
+        time_per_step = duration / steps
+        
+        for i in range(steps + 1):
+            servo.angle = start_angle + (angle_increment * i)
+            time.sleep(time_per_step)
--- src/main.py
+++ src/main.py
@@ -1,71 +1,19 @@
-from adafruit_servokit import ServoKit
 import typer
-import threading
 import time
 import pygame
 import board
 import digitalio
 import dispenser
+import animatronic
 from audio_player import AudioPlayer
 from version import __version__
 
 audio_player = AudioPlayer()
 
-kit = ServoKit(channels=16)
-
-mouth = kit.servo[0]
-mouth_movement_delay = 0.2
-mouth_closed_angle = 70
-mouth_open_angle = 180
-
-arm = kit.servo[1]
-arm_start = 0
-arm_end = 10
-arm_duration = 0.5
-arm_steps = 200
-arm_delay = 1
-
+animatronic = animatronic.GSBody(0, 1)
 card = dispenser.GSCardDispenser(board.D17, board.D18, board.D19, board.D20)
 
 app = typer.Typer(help="Goblin Speaks fortune teller software")
-
-def smooth_servo_movement(servo, start_angle, end_angle, duration, steps=50):
-    """
-    Moves servo smoothishly from a start angle to an end angle over a specified duration.
-    """
-    angle_increment = (end_angle - start_angle) / steps
-    time_per_step = duration / steps
-    
-    for i in range(steps + 1):
-        servo.angle = start_angle + (angle_increment * i)
-        time.sleep(time_per_step)
-
-def talk_animation(duration):
-    """
-    Runs the jaw animation open/close loop for a specific duration in seconds.
-    """
-    print("Starting mouth animation...")
-
-    start_time = time.time()
-    
-    while (time.time() - start_time) < duration:
-        mouth.angle = mouth_open_angle
-        time.sleep(mouth_movement_delay)
-        
-        mouth.angle = mouth_closed_angle
-        time.sleep(mouth_movement_delay)
-
-def arm_animation(druation):
-    """
-    Runs the arm animation loop for a specific duration in seconds.
-    """
-    print("Starting arm animation...")
-
-    while (time.time() - start_time) < duration:
-        smooth_servo_movement(arm, arm_start, arm_end, arm_duration, arm_steps)
-        time.sleep(1)
-        smooth_servo_movement(arm, arm_end, arm_start, arm_duration, arm_steps)
-        time.sleep(1)
 
 def play():
     """
@@ -77,23 +25,7 @@ def play():
     print("Starting play...")
 
     duration = audio_player.playrandom()
-    
-    talk_thread = threading.Thread(
-        target=talk_animation, 
-        args=(duration,), 
-        daemon=True
-    )
-    talk_thread.start()
-
-    arm_thread = threading.Thread(
-        target=arm_animation,
-        args=(duration,),
-        daemon=True
-    )
-    arm_thread.start()
-        
-    talk_thread.join()
-    arm_thread.join()
+    animatronic.animate(duration)
 
     print("Audio/animations complete...")
     
