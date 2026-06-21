ba00df46fbbce519b91b24f1ca090a22729643d7
James Hogle <jhogle117@gmail.com>
2026-06-21 16:03:47 -0500

    make speed and steps configurable

--- src/dispenser/gs_card.py
+++ src/dispenser/gs_card.py
@@ -16,10 +16,12 @@ class GSCardDispenser(Dispenser):
         pin4 = config.get('dispenser.gs_card.pin_4', board.D22)
 
         self.motor = motor.Stepper(pin1, pin2, pin3, pin4)
+        self.steps = config.get('dispenser.gs_card.steps', 4096)
+        self.delay = config.get('dispenser.gs_card.delay', 0.001)
 
     def dispense(self):
         """
         Dispenses a fortune card.
         """
         print("Dispensing card...")
-        self.motor.move_backward(2800)
+        self.motor.move_backward(self.steps, self.delay)
