2682cf0eb0a5ab283dd95265a9d442004ac2b486
James Hogle <jhogle117@gmail.com>
2026-06-22 22:55:25 -0500

    only init components for commands that need it

--- src/main.py
+++ src/main.py
@@ -13,12 +13,14 @@ app = typer.Typer(help="Goblin Speaks fortune teller software")
 
 @app.command()
 def run():
+    player = load_player()
     print("Not yet implemented. Shutting down...")
     player.shutdown()
     sys.exit(0)
 
 @app.command()
 def test():
+    player = load_player()
     menu = TestMenu(player)
     menu.run()
     player.shutdown()
@@ -31,14 +33,15 @@ def update():
     """
     perform_update()
 
-if __name__ == "__main__":
+def load_player():
     audio_player = AudioPlayer()
     animatronic_instance = animatronic.AnimatronicFactory.create()
     dispenser_instance = dispenser.DispenserFactory.create()
     activator_instance = activator.ActivatorFactory.create()
-    
-    player = DefaultPlayer(audio_player, animatronic_instance, dispenser_instance, activator_instance)
 
+    return DefaultPlayer(audio_player, animatronic_instance, dispenser_instance, activator_instance)
+
+if __name__ == "__main__":
     print(r"""
      _____       _     _ _         _____                  _        
     |  __ \     | |   | (_)       /  ___|                | |       
